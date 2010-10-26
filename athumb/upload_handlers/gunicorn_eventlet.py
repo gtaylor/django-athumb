313def44dc2f000eb604aa972d8a67ac67e1eef4
Greg Taylor <gtaylor@duointeractive.com>
2010-10-26 15:36:32 -0400

    Adding an upload_handlers module with a very slightly modified upload handler for those using gunicorn + eventlet workers and very large file uploads (over 10 MB).

---
+++ athumb/upload_handlers/gunicorn_eventlet.py
@@ -0,0 +1,21 @@
+"""
+Upload handlers with small tweaks to work with gunicorn + eventlet async
+workers. These should eventually becom unecessary as gunicorn continues
+to improve.
+"""
+from django.core.files.uploadhandler import TemporaryFileUploadHandler
+import eventlet
+
+class EventletTmpFileUploadHandler(TemporaryFileUploadHandler):
+    """
+    Uploading large files can cause a worker thread to hang long enough to
+    hit the timeout before the upload can be completed. Sleep long enough
+    to hand things back to the other threads to avoid a timeout.
+    """
+    def receive_data_chunk(self, raw_data, start):
+        """
+        Over-ridden method to circumvent the worker timeouts on large uploads.
+        """
+        self.file.write(raw_data)
+        # CHANGED: This un-hangs us long enough to keep things rolling.
+        eventlet.sleep(0)
