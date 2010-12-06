8f212d751eb702997b890a38284642891c39eb73
Greg Taylor <gtaylor@duointeractive.com>
2010-12-06 17:33:20 -0500

    Improvements to help with correct south introspection.

--- athumb/fields.py
+++ athumb/fields.py
@@ -15,6 +15,13 @@ from django.core.cache import cache
 from manipulations import generate_thumb_basic, sorl_scale_and_crop
 from validators import ImageUploadExtensionValidator
 
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^athumb\.fields\.ImageWithThumbsField"])
+except ImportError:
+    # Not using South, no big deal.
+    pass
+
 # Cache URLs for thumbnails so we don't have to keep re-generating them.
 THUMBNAIL_URL_CACHE_TIME = getattr(settings, 'THUMBNAIL_URL_CACHE_TIME', 3600 * 24)
 
@@ -155,61 +162,24 @@ class ImageWithThumbsFieldFile(ImageFieldFile):
         super(ImageWithThumbsFieldFile, self).delete(save)
 
 class ImageWithThumbsField(ImageField):
-    attr_class = ImageWithThumbsFieldFile
     """
     Usage example:
     ==============
     photo = ImageWithThumbsField(upload_to='images', thumbs=((125,125),(300,200),)
-    
-    To retrieve image URL, exactly the same way as with ImageField:
-        my_object.photo.url
-    To retrieve thumbnails URL's just add the size to it:
-        my_object.photo.url_125x125
-        my_object.photo.url_300x200
-    
+        
     Note: The 'thumbs' attribute is not required. If you don't provide it, 
     ImageWithThumbsField will act as a normal ImageField
-        
-    How it works:
-    =============
-    For each size in the 'sizes' atribute of the field it generates a 
-    thumbnail with that size and stores it following this format:
-    
-    available_filename.[width]x[height].extension
-
-    Where 'available_filename' is the available filename returned by the storage
-    backend for saving the original file.
-    
-    Following the usage example above: For storing a file called "photo.jpg" it saves:
-    photo.jpg          (original file)
-    photo.125x125.jpg  (first thumbnail)
-    photo.300x200.jpg  (second thumbnail)
-    
-    With the default storage backend if photo.jpg already exists it will use these filenames:
-    photo_.jpg
-    photo_.125x125.jpg
-    photo_.300x200.jpg
-    
-    With the S3BotoStorage backends, the default is to over-write existing files.
-    
-    Note: django-athumb assumes that if filename "any_filename.jpg" is available 
-    filenames with this format "any_filename.[width]x[height].jpg" will be available, too.
     """
-    def __init__(self, verbose_name=None, name=None, thumbnail_format=None,
-                 width_field=None, height_field=None, thumbs=(), **kwargs):
-        self.verbose_name = verbose_name
-        self.name = name
-        self.width_field = width_field
-        self.height_field = height_field
-        self.thumbs = thumbs
-        self.thumbnail_format = thumbnail_format
-        super(ImageField, self).__init__(validators=[IMAGE_EXTENSION_VALIDATOR], **kwargs)
+    attr_class = ImageWithThumbsFieldFile
 
-    def south_field_triple(self):
-        """
-        Return a suitable description of this field for South.
-        """
-        from south.modelsinspector import introspector
-        field_class = 'django.db.models.fields.files.ImageField'
-        args, kwargs = introspector(ImageField)
-        return (field_class, args, kwargs)
+    def __init__(self, *args, **kwargs):
+        self.thumbs = kwargs.pop('thumbs', ())
+        self.thumbnail_format = kwargs.pop('thumbnail_format', None)
+
+        if not kwargs.has_key('validators'):
+            kwargs['validators'] = [IMAGE_EXTENSION_VALIDATOR]
+            
+        if not kwargs.has_key('max_length'):
+            kwargs['max_length'] = 255
+
+        super(ImageWithThumbsField, self).__init__(*args, **kwargs)
